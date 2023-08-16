1bcccf3c4b28c5cd74a29e68f8260cfa7fb54552
primaryseargent <akverma1605@gmail.com>
2023-08-16 11:42:42 +0530

    Commited first function for backward command

--- Chandryaan3.js
+++ Chandryaan3.js
@@ -26,4 +26,27 @@ class Chandrayaan3 {
         break;
     }
   }
+  //function for move backward command
+  moveBackward() {
+    switch (this.direction) {
+      case "N":
+        this.position.y -= 1;
+        break;
+      case "S":
+        this.position.y += 1;
+        break;
+      case "E":
+        this.position.x -= 1;
+        break;
+      case "W":
+        this.position.x += 1;
+        break;
+      case "Up":
+        this.position.z -= 1;
+        break;
+      case "Down":
+        this.position.z += 1;
+        break;
+    }
+  }
 }
